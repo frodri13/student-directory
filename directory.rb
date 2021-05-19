74583d203d01ceb96ca7d095c202091c152f6d01
Fabio <alessf13@gmail.com>
2021-05-19 15:25:33 +0100

    Comments added to the file

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# List of students printed
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# then, we print the total number of students
 print "Overall, we have "
 print 11
 print " great students"
