140259752fd708e2bce60cc50cb7a9f604df869f
Fabio <alessf13@gmail.com>
2021-05-22 18:41:58 +0100

    Interactive menu added.

--- directory.rb
+++ directory.rb
@@ -1,8 +1,8 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students.".center(70)
   puts "To finish, just hit the return key twice".center(68)
-  # create an empty array
-  students = []
   # get the first name. Alternative to chomp: to_s.slice!(/\w+/)
   name = gets.chomp
   # if name is empty, the program will stop and it will put the following message and stop.
@@ -31,7 +31,7 @@ def input_students
     puts "What's their hobby?".center(70)
     hobby = gets.chomp
     # all variables are paired with a symbol and sent as an array 
-    students << {name: name, cohort: cohort, age: age, country: country, hobby: hobby}
+    @students << {name: name, cohort: cohort, age: age, country: country, hobby: hobby}
 
 
     puts "Are you happy with the information you've provided us? if you would like to edit it, please type 'no'.".center(70)
@@ -43,24 +43,24 @@ def input_students
       puts "Please retype your data:".center(70)
       change = gets.chomp
 
-      students[students.count - 1][category.to_sym] = change
+      @students[@students.count - 1][category.to_sym] = change
       puts "Would you like to edit another category?".center(70)
       answer = gets.chomp
       break if answer == 'no'
 
     end
   
-    if students.count < 2
-      puts "Now we have #{students.count} student".center(70)
+    if @students.count < 2
+      puts "Now we have #{@students.count} student".center(70)
     else
-      puts "Now we have #{students.count} students".center(70)
+      puts "Now we have #{@students.count} students".center(70)
     end
     # get another name from the user
     puts "What's the next student's name?".center(70)
     name = gets.chomp
   end
   # return the array of students
-students
+@students
 end 
 
 def print_header
@@ -68,19 +68,8 @@ def print_header
   puts "-------------".center(70)
 end
 
-def print(students)
-  # count = 0
-  # while count < students.count do
-  #   name = students[count][:name].to_s
-  #   cohort = students[count][:cohort].to_s
-  #   age = students[count][:age].to_s
-  #   country = students[count][:country].to_s
-  #   hobby = students[count][:hobby].to_s
-
-  #   puts "#{count += 1}. #{name} (#{cohort} cohort)".center(70)
-  #   puts "Age: #{age}. Country of origin: #{country}. Hobby: #{hobby}".center(70)
-
-  # end
+def print_students_list(students)
+  # add functionality to print all
   puts "Please select the cohort you would like to print:".center(70)
   cohort = gets.chomp
   count = 0
@@ -100,7 +89,34 @@ def print_footer(student)
   end
 end
 
-students = input_students
-print_header
-print(students) 
-print_footer(students)
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" 
+end
+
+def show_students
+  print_header
+  print_students_list(@students)
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interactive_menu
