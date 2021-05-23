cddc5a3c12d047144313a8411b94a85339098c4c
Fabio <alessf13@gmail.com>
2021-05-23 19:22:03 +0100

    Refactoring of longer methods

--- directory.rb
+++ directory.rb
@@ -1,68 +1,97 @@
 @students = []
-
-def input_students
+# This interactive menu will be the first thing appearing after loading the file in the command line
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit" 
+end
+# pre input_students methods:
+# Initial greeting to the user asking them to insert the students' name
+def students_name
   puts "Please enter the names of the students.".center(70)
   puts "To finish, just hit the return key twice".center(68)
   # get the first name. Alternative to chomp: to_s.slice!(/\w+/)
   name = STDIN.gets.chomp
-  # if name is empty, the program will stop and it will put the following message and stop.
   if name.empty?
-    puts "THE HEROES ARE WINNING! OUR SCHOOL IS EMPTY!! NO STUDENTS THIS YEAR!!!".center(75)
-    exit
+    false
+  else
+    @name = name
   end
+end
+
+def students_age
+  puts "What's their age?".center(70)
+  @age = STDIN.gets.chomp
+end
+
+def students_cohort
+  puts "Which cohort will they be attending?".center(70)
+  @cohort = STDIN.gets.chomp
+  @cohort = "2021" if @cohort.length == 0
+end
+
+
+def students_country
+  puts "Birth country?".center(70)
+  @country = STDIN.gets.chomp
+end
+
+def students_hobby
+  puts "What's their hobby?".center(70)
+  @hobby = STDIN.gets.chomp
+end
+
+def students_hash_to_array
+  @students << {name: @name, cohort: @cohort, age: @age, country: @country, hobby: @hobby}
+end
+
+def edit_student_intake
+  puts "Are you happy with the information you've provided us? if you would like to edit it, please type 'no'.".center(70)
+  happy = STDIN.gets.chomp
+
+  while happy == 'no' do
+    puts "Type the category you would like to change [name, cohort, age, country, hobby]:".center(70)
+    category = STDIN.gets.chomp
+    puts "Please retype your data:".center(70)
+    change = STDIN.gets.chomp
+
+    @students[@students.count - 1][category.to_sym] = change
+    puts "Would you like to edit another category?".center(70)
+    answer = STDIN.gets.chomp
+    break if answer == 'no'
+  end
+end
+
+def students_count
   # while the name is not empty, repeat this code
-  while !name.empty? do
-    puts "Which cohort will they be attending?".center(70)
-    cohort = STDIN.gets.chomp
-    cohort = "2021" if cohort.length == 0
-    puts "What's their age?".center(70)
-    age = STDIN.gets.chomp
-    until age.to_i > 0
-      puts "Please enter a number".center(70)
-      age = STDIN.gets.chomp
-        if age.to_i > 0 && age.to_i < 18
-          puts "You're too young to study at the Villains Academy. Please come back when you're older.".center(70)
-          exit
-        end
-    end
-  
-    puts "Birth country?".center(70)
-    country = STDIN.gets.chomp
-    puts "What's their hobby?".center(70)
-    hobby = STDIN.gets.chomp
-    # all variables are paired with a symbol and sent as an array 
-    @students << {name: name, cohort: cohort, age: age, country: country, hobby: hobby}
-
-
-    puts "Are you happy with the information you've provided us? if you would like to edit it, please type 'no'.".center(70)
-    happy = STDIN.gets.chomp
-    
-    while happy == 'no' do
-      puts "Type the category you would like to change [name, cohort, age, country, hobby]:".center(70)
-      category = STDIN.gets.chomp
-      puts "Please retype your data:".center(70)
-      change = STDIN.gets.chomp
-
-      @students[@students.count - 1][category.to_sym] = change
-      puts "Would you like to edit another category?".center(70)
-      answer = STDIN.gets.chomp
-      break if answer == 'no'
-
-    end
-  
-    if @students.count < 2
-      puts "Now we have #{@students.count} student".center(70)
-    else
-      puts "Now we have #{@students.count} students".center(70)
-    end
-    # get another name from the user
-    puts "What's the next student's name?".center(70)
-    name = STDIN.gets.chomp
+  if @students.count < 2
+    puts "Now we have #{@students.count} student".center(70)
+  else
+    puts "Now we have #{@students.count} students".center(70)
   end
-  # return the array of students
-@students
-end 
+  # get another name from the user
+  puts "What's the next student's name?".center(70)
+  name = STDIN.gets.chomp
+end
 
+# 1. Input the students:
+def input_students
+  loop do  
+    break if students_name == false
+    students_cohort
+    students_age
+    students_country
+    students_hobby
+    edit_student_intake
+    students_hash_to_array
+    students_count
+  end
+  @students
+end
+
+# 2. Show the students:
 def print_header
   puts "The students of Villains Academy".center(70)
   puts "-------------".center(70)
@@ -82,42 +111,12 @@ def print_footer
   end
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit" 
-end
-
 def show_students
   print_header
   print_students_list
   print_footer
 end
-
-def process(selection)
-  case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "3"
-    save_students
-  when "4"
-    load_students
-  when "9"
-    exit
-  end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
-
+# 3. Save students:
 def save_students
   # open the file for writing
   file = File.open("students.csv", "w")
@@ -129,12 +128,12 @@ def save_students
   end
   file.close
 end
-
+# 4. Load students:
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
-    name, cohort, age, country, hobby = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym, age: age, country: country, hobby: hobby}
+    @name, @cohort, @age, @country, @hobby = line.chomp.split(',')
+    students_hash_to_array
   end
   file.close
 end
@@ -150,6 +149,32 @@ def try_load_students
     exit # quit the program
   end
 end
+# This method describes every functionality of the print_menu
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "9"
+    exit
+  end
+end
+# print_menu and process are put in a continuous loop:
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
 
 try_load_students
 interactive_menu
+
+# After we added the code to load the students from file, we ended up with adding the students to @students in two places.
+# The lines in load_students() and input_students() are almost the same. 
+# This violates the DRY (Don't Repeat Yourself) principle. How can you extract them into a method to fix this problem?
